38ab8f513a020455801351c639e966dce0fafd18
Evg-msk <jek1544@mail.ru>
2020-03-16 21:19:32 +0300

    Add example Modules

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-const count = '->Это результат вывода переменной count!<-';
-const logic = true;
-const trueword = false;
+import {count, length} from './number/number';
 
 function App() {
   return (
@@ -16,15 +13,12 @@ function App() {
         }}> App by Evgeny_M </h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p style={{
-          color: 'cyan'
+          fontSize: 40,
+          color: "salmon",
         }}>
-          {count}
+          Здесь вы видете результат "count * length = <strong style={{color: 'darkgoldenrod'}}> {count * length}" </strong>
         </p>
-        <p> {25} </p>
-        <p> {1024 - 524} </p>
-        <p style={{color: 'brown'}}> {logic && 'logic operation'} </p>
-        <p style={{color: 'green'}}> {trueword ? 'Если вы увидели это сообщение, то правда Ваша!' : 'Похоже кто-то изменил параметр на сладкую ложь'} </p>
-        <p> Вывод "undefined:__" {undefined} , "null:__" {null}, "false:__" {false}, "true:__" {true}. </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/number/number.js
@@ -0,0 +1,4 @@
+const count = 25;
+const length = 110;
+
+export  {count, length}
