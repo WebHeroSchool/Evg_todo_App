a21e1c2cb9d22c103129592f0847d04b63f8d447
Evg-msk <jek1544@mail.ru>
2020-03-18 20:47:17 +0300

    add react-components to index

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
 
+const ItemList = () => (
+    <ul style={{fontSize: 30}}>
+        <li> 1. </li>
+        <li> 2. </li>
+        <li> 3. </li>
+    </ul>
+);
 const elem = (
-    <div style={{
+    <h1 style={{
       color: 'burlywood',
       fontSize: 50,
       textAlign: 'center'
     }}>
-      React_App by <strong> Evgeny Morozov! </strong>
+      ToDo_App by <strong> Evgeny Morozov! </strong>
+    </h1>
+);
+
+const App = () => (
+    <div>
+        {elem}
+        <ItemList />
     </div>
 );
 
-export {elem};
+export default App;
