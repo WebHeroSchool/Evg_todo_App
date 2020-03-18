4f6e23a0b1a6e828d82e1e3b741603a44b97df25
Evg-msk <jek1544@mail.ru>
2020-03-18 21:59:17 +0300

    create structure prj

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Footer = () => <p> Some footer elements </p>;
+
+    export default Footer;
---
+++ src/Components/Item/Item.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Item = () => (
+    <span> Выпить утром чашечку кофе! </span>
+);
+
+export default Item;
