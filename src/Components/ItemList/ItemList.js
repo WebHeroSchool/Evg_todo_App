02a379c1825ad2f3a7d88dd2b3a5135da05a47a4
Evg-msk <jek1544@mail.ru>
2020-03-31 14:47:26 +0300

    add event on DeleteIcon

--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -4,20 +4,22 @@ import Checkbox from "@material-ui/core/Checkbox";
 import DeleteIcon from '@material-ui/icons/Delete';
 import styles from "../App/App.module.css";
 
-const deltask = () => alert('Deleted!');
-const ItemList = ({items, onClickDone}) => (
+const ItemList = ({items, onClickDone, onClickDelete}) => (
     <ul>
         {items.map(item => <li key={item.task}>
             <Checkbox
                 checked={item.isDone}
-              onClick={() => onClickDone(item.id)}
+                onClick={() => onClickDone(item.id)}
             />
             <Item
                 task={item.task}
                 isDone={item.isDone}
                 id={item.id}
             />
-            <DeleteIcon className={styles.butdelete} onClick={deltask}/>
+            <DeleteIcon
+                className={styles.butdelete}
+                onClick={() => onClickDelete(item.id)}
+            />
         </li>)}
 
     </ul>
